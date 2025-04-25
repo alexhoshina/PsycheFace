d36d1ac4c687f394fd1a4aae06ab4831aa5cec4c
Hoshina <salexhoshina@users.noreply.github.com>
2025-04-25 14:15:17 +0800

    fix: 修复错误配置的模型输入大小
    - 在 .gitignore 中添加 Nuxt 目录

--- app/src/models/mock.py
+++ app/src/models/mock.py
@@ -5,20 +5,20 @@ from src.models.base import EmotionRecognitionModel, FaceDetectionModel
 from src.models.factory import ModelFactory
 
 
-@ModelFactory.register_detector("mockD", model_path="null", input_shape=(100, 100))
+@ModelFactory.register_detector("mockD", model_path="null")
 @ModelFactory.register_detector("mockD2", model_path="null")
 class MockDetector(FaceDetectionModel):
     """模拟人脸检测"""
-    def __init__(self, model_path: str, input_shape: tuple):
-        print(input_shape)
+    def __init__(self, model_path: str):
         pass
     def detect_faces(self, image: np.ndarray) -> List[Tuple[int, int, int, int]]:
         return [(0, 0, 0, 0)]
 
-@ModelFactory.register_recognizer("mockR", model_path="null")
+@ModelFactory.register_recognizer("mockR", model_path="null", input_shape=(100, 100))
 class MockRecognizer(EmotionRecognitionModel):
     """模拟表情识别"""
-    def __init__(self, model_path: str):
+    def __init__(self, model_path: str, input_shape: tuple):
+        print(input_shape)
         pass
     def preprocess_image(self, image: np.ndarray) -> np.ndarray:
         pass
